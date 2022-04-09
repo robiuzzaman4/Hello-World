7b7e61704d44def950b819860a1d788599cd329e
robiuzzaman4 <robiuzzaman4@gmail.com>
2022-04-09 11:34:58 +0600

    Hello World is my first repository

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+    result.innerHTML="Hello World";
+})
